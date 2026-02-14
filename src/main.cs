917868a38c48117e688e0de2c4244cb624c6089a
Ovadiah <230772997+randomcat962codes@users.noreply.github.com>
2026-02-14 12:28:56 -0600

    Collect files and get their size.

--- src/main.cs
+++ src/main.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter the directories you want to be searched. Enter \"end\" when you are finished.");
+
+        bool getDirs = true;
+        int dirCount = 0; // Iterator
+        Dictionary<string, long> dirCollection = new(); // Contains the directories with their respoctive sizes.
+        while (getDirs)
+        {
+            Console.Write(Convert.ToString(dirCount + 1) + ": ");
+            string input = Console.ReadLine();
+
+            // Processes the input.
+            if (input == "end")
+            {
+                getDirs = false;
+            } 
+            else
+            {
+                DirectoryInfo dirInfo = new(input);
+
+                // Makes sure the listed directory exists.
+                if (!dirInfo.Exists) 
+                {
+                    Console.WriteLine("The directory could not be found.");
+                    continue;
+                }
+
+                long totalSize = 0;
+
+                try
+                {
+                    // Gets the file size and adds it to the collection.
+                    IEnumerable<FileInfo> files = dirInfo.EnumerateFiles("*", SearchOption.AllDirectories);
+                    totalSize = files.Sum(file => file.Length);
+                    dirCollection[input] = totalSize;
+                    dirCount++;
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine($"\t*There was trouble searching the directory. The error will be listed below:\n{err.Message}");
+                }
+            }
+        }
+
     }
 }

2fff650a6806aa1edb8b7a52a0ce7eaa697b9cd8
Ovadiah <230772997+randomcat962codes@users.noreply.github.com>
2026-02-14 13:32:02 -0600

    Convert the sizes to readable formats.

--- src/main.cs
+++ src/main.cs
@@ -50,15 +50,46 @@ class Program
             }
         }
 
-        Console.WriteLine("\nResults\n---------------------------"); //Creates a line break.
-
-        // Displays the size of the directories.
         Dictionary<string, long>.KeyCollection directories = dirCollection.Keys;
+
+        //Procceses the sizes to convert them to MG or GB if possible.
+        Dictionary<string, string> results = new();
 
         foreach (string dir in directories)
         {
-            string size = Convert.ToString(dirCollection[dir]);
-            Console.WriteLine($"{dir}: {size}");
+            // Constants for byte conversion.
+            const int KILOBYTE = 1000;
+            const int MEGABYTE = 1000000;
+            const int GIGABYTE = 1000000000;
+
+            long size = dirCollection[dir];
+            
+            if (size >= KILOBYTE && size < MEGABYTE) // KB
+            {
+                results[dir] = Convert.ToString(size / KILOBYTE) + " KB";
+            }
+            else if (size >= MEGABYTE && size < GIGABYTE) // MB
+            {
+                results[dir] = Convert.ToString(size / MEGABYTE) + " MB";
+            }
+            else if (size >= GIGABYTE) // GB
+            {
+                results[dir] = Convert.ToString(size / GIGABYTE) + " GB";
+            }
+            else // Byte
+            {
+                results[dir] = Convert.ToString(size) + " Bytes";
+            }
+        }
+
+        Console.WriteLine("\nResults\n---------------------------"); //Creates a line break.
+
+        // Displays the size of the directories.
+        Dictionary<string, string>.KeyCollection resultKeys = results.Keys;
+
+        foreach (string dir in resultKeys)
+        {
+            Console.WriteLine($"{dir}: {results[dir]}");
         }
     }
 }

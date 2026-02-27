2787fc3873061b268abe6463c4c96d701cc87216
Ovadiah <230772997+randomcat962codes@users.noreply.github.com>
2026-02-27 09:31:36 -0600

    Add a button for getting the directory input

--- gui/MainPage.xaml.cs
+++ gui/MainPage.xaml.cs
@@ -6,4 +6,14 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void CompileData(object sender, RoutedEventArgs e)
+	{
+		// Gets the directory/file paths
+		string editorContent = DirectoryInputText.Text;
+		string[] paths = editorContent.Split(
+			new string[] {"\r\n", "\n"},
+			StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries
+		);
+	}
 }

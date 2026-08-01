da31ad5a4d26a86c045351197431c2995d8af9f1
Landon <230772997+landon-codes@users.noreply.github.com>
2026-08-01 01:03:54 +0000

    Create a base structure for the program A couple of functions were added that return the size of dirs or files provided. They do not have implementation yet.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 use std::path::Path;
 
+fn get_file_size(file: &Path) -> i64 {
+    0 // Temprorary value until implementation
+}
+
+fn get_dir_size(file: &Path) -> i64 {
+    0 // Temprorary value until implementation
+}
+
 fn main() {
+    let mut file_compilations: HashMap<&str, i64> = HashMap::new();
     let dirs_and_files: Vec<String> = env::args().collect();
 
     for p in dirs_and_files {
@@ -11,6 +21,14 @@ fn main() {
 
         if !content.exists() {
             panic!("The path {} does not exist!", p);
+        }
+
+        let mut content_size: i64;
+
+        if content.is_file() {
+            content_size = get_file_size(content);
+        } else if content.is_dir() {
+            content_size = get_dir_size(content);
         }
 
         

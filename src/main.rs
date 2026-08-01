4ec2fa8cff05f4aa9df3d3048699734cab3c8ea8
Landon <230772997+landon-codes@users.noreply.github.com>
2026-08-01 00:24:31 +0000

    Take command line arguments for paths

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let dirs_and_files: Vec<String> = env::args().collect();
+
+    for p in dirs_and_files {
+        // Get the path
+        let content = Path::new(&p);
+
+        if !content.exists() {
+            panic!("The path {} does not exist!", p);
+        }
+
+        
+    }
 }
